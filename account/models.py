a8b1234b803f0108e179350f7cf7986113f64feb
mmd on laptop <mmdvafaj@gmail.com>
2025-06-29 11:00:12 +0430

    add CustomUser model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomUser(models.User):
+    bio = models.CharField(max_length=255)
+    profile = models.ImageField(upload_to='user_profiles')
