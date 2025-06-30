3ab21f978019efe3526f0587bfb332ad2d346b62
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-06-30 21:39:16 +0330

    add CustomUserViewSet

--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from account.serializers import CustomUserSerializer
+from account.models import CustomUser
 
-# Create your views here.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
