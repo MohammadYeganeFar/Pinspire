f32dc9cd21868228d37f14033ff8565eed1aa39e
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-06-30 22:47:53 +0330

    update model CustomUser

--- account/models.py
+++ account/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class CustomUser(User):
-    bio = models.CharField(max_length=255)
-    profile = models.ImageField(upload_to='user_profiles')
+    bio = models.CharField(max_length=255, null=True, blank=True)
+    profile = models.ImageField(upload_to='user_profiles', null=True, blank=True)
     following = models.ManyToManyField('self',
                                     related_name='followers', 
                                     symmetrical=False,
