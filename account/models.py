9cbc97c45e5c19bb759b52d6e58a22ca779f71ad
mmd on laptop <mmdvafaj@gmail.com>
2025-06-30 11:40:01 +0430

    feature: add field following to CustomUser model for following operation

--- account/models.py
+++ account/models.py
@@ -5,12 +5,8 @@ from django.contrib.auth.models import User
 class CustomUser(User):
     bio = models.CharField(max_length=255)
     profile = models.ImageField(upload_to='user_profiles')
-    following = models.ManyToManyField(self, through='Follow', related_name='followers', symmetrical=False
+    following = models.ManyToManyField('self',
+                                    related_name='followers', 
+                                    symmetrical=False,
+                                    blank=True)
 
-class Follow(models.Model):
-    from_user = models.ForeignKey(CustomUser, 
-                                                        on_delete=models.CASCADE, 
-                                                        related_name='rel_from_set')
-    to_user = models.ForeignKey(CustomUser, 
-                                                        on_delete=models.CASCADE, 
-                                                        related_name='rel_to_set')
