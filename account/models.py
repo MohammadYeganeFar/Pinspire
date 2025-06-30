b731cbe4a926e3055424d779156b52831dac57f5
mmd on laptop <mmdvafaj@gmail.com>
2025-06-30 11:11:00 +0430

    remove migrations

--- account/models.py
+++ account/models.py
@@ -5,3 +5,12 @@ from django.contrib.auth.models import User
 class CustomUser(User):
     bio = models.CharField(max_length=255)
     profile = models.ImageField(upload_to='user_profiles')
+    following = models.ManyToManyField(self, through='Follow', related_name='followers', symmetrical=False
+
+class Follow(models.Model):
+    from_user = models.ForeignKey(CustomUser, 
+                                                        on_delete=models.CASCADE, 
+                                                        related_name='rel_from_set')
+    to_user = models.ForeignKey(CustomUser, 
+                                                        on_delete=models.CASCADE, 
+                                                        related_name='rel_to_set')
