7bb71ce68fc1b55c79558185c3f362de9b9e4605
mmd on laptop <mmdvafaj@gmail.com>
2025-07-01 15:13:21 +0430

    update PinViewSet: overwrite list()

--- core/views.py
+++ core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from core.models import Pin, Board
 from core.serializers import PinSerializer, BoardSerializer
 
 class PinViewSet(viewsets.ModelViewSet):
     serializer_class = PinSerializer
     queryset = Pin.objects.all()
+    
+    def list(self, request):
+        pins = Pin.objects.filter(visibility='PU')
+        serializer = self.get_serializer(pins, many=True)
+        return Response(serializer.data)
+        
 
 
 class BoardViewSet(viewsets.ModelViewSet):
