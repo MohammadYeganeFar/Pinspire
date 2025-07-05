a471701de2a1c3a21ca226f15e5f03b76cd73c05
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-07-05 15:56:05 +0330

    add basic filter, search and ordering features

--- shop_manage/views.py
+++ shop_manage/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import P
+from django_filters.rest_framework import (DjangoFilterBackend,
+                                            )
 from shop_manage.serializers import (ProductSerializer, 
                                     CategorySerializer, 
                                     OrderSerializer)
@@ -12,8 +15,15 @@ from shop_manage.filters import ProductFilter
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = (Product.objects.all())
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, 
+                       SearchFilter, 
+                       OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['name','description','category__name']
+    search_param = 'q'
+    # ordering_fields = ['name', 'price', 'is_available', 'created_at']
+    ordering_fields = '__all__'
+    ordering = ['name']
     
     def list(self, request, *args, **kwargs):
         products = self.filter_queryset(self.get_queryset())
